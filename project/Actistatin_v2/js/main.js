be0c66a27f4817005fc4aa43d9981c8955cd390b
Sergey Balakirev <balakirev.sergey87@gmail.com>
2020-03-14 13:31:24 +0200

    first commit

---
+++ project/Actistatin_v2/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: false,
+		
+  		autoplay: true,
+  		autoplaySpeed: 3000,
+  		infinite: true,
+  		speed: 700,
+  		fade: true,
+  		cssEase: 'linear'
+	});
+});
+
+
+
+
+
